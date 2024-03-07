e56bcf4dd9f9c9084db329a27af083dc451b3b39
Sergey020 <sergeytkachenko020@gmail.com>
2024-03-07 11:39:55 +0200

    Update task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+const categoriesList = document.querySelector(`#categories`);
+console.log(categoriesList);
+const categoriesItems = categoriesList.querySelectorAll('.item');
+console.log(`Количество категорий: ${categoriesItems.length}`);
+categoriesItems.forEach(category => {
+    
+})
