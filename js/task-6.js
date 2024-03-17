c2434dc7fc27258f8ff76d6b6a577d0a7118f7c6
Sergey020 <sergeytkachenko020@gmail.com>
2024-03-17 14:50:42 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputEl = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+function createBoxes(amount){
+if(amount >= 1 && amount <= 100) {
+  let str = '';
+  for (let i = 1; i <= amount; i++) {
+    str += `<div></div>`;
+  }
+  boxesEl.innerHTML = `${str}`;
+}
+return boxesEl;
+}
+function handleClick() {
+createBoxes(amount);
+}
+btnCreate.addEventListener('click', handleClick);
+inputEl.addEventListener('input', event => amount = event.currentTarget.value);
