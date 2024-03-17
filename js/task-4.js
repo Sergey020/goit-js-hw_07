be63edc58d866ec5f023f99a5a8d4a647e18518c
Sergey020 <sergeytkachenko020@gmail.com>
2024-03-17 12:59:24 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,10 @@
-
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password= form.elements.password.value;
+    console.log(`${email}, ${password}`);
+    form.reset();
+}
