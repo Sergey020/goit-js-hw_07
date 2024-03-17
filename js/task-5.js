787ce288220948bdbac3e9818ad52216378f0dc0
Sergey020 <sergeytkachenko020@gmail.com>
2024-03-17 13:53:26 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+const divEl = document.querySelector('.widget');
+const bodyEl = divEl.parentNode;
+function handleClick(event){
+  console.log(getRandomHexColor());
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+btnEl.addEventListener('click',handleClick);
